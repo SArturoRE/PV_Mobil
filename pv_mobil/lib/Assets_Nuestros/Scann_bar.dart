12e5664f0229b35fbffa7ed010c72f3d6f863e22
Salvador Arturo Ramirez Escamilla <salvador.ramireze@alumno.buap.mx>
2021-02-25 08:56:56 -0600

    primer widgeth
    Separamos la lectura de códigos en un widget independiente el cual llamamos desde la pagina principal, posteriormente aremos el llamado mediante un botón.

--- pv_mobil/lib/Assets_Nuestros/Scann_bar.dart
+++ pv_mobil/lib/Assets_Nuestros/Scann_bar.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-class Scanear extends StatelessWidget {
+class Scanear extends StatefulWidget {
+  Scanear({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Scanear> {
+  String _counter, value = "";
+
+  Future _incrementCounter() async {
+    _counter = await FlutterBarcodeScanner.scanBarcode(
+        "#004297", "Cancel", true, ScanMode.DEFAULT);
+
+    setState(() {
+      value = _counter;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Contenido de lo escaneado:',
+            ),
+            Text(
+              '$value',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.settings_overscan),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
--- pv_mobil/lib/main.dart
+++ pv_mobil/lib/main.dart
@@ -18,57 +18,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/Scaneo': (context) => Scanear(),
       },
-      home: MyHomePage(title: 'PV_mobil'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _counter, value = "";
-
-  Future _incrementCounter() async {
-    _counter = await FlutterBarcodeScanner.scanBarcode(
-        "#004297", "Cancel", true, ScanMode.DEFAULT);
-
-    setState(() {
-      value = _counter;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Contenido de lo escaneado:',
-            ),
-            Text(
-              '$value',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.settings_overscan),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scanear(title: 'PV_mobil'),
     );
   }
 }
